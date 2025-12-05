a02e24fe048fdd6bb79e0da746072e6c29b6feec
frutigeraerick <erei8377@gmail.com>
2025-12-05 10:57:38 -0500

    [+] main Jinja2 and connections

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
+import os
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
 
-app = FastAPI(title="sigmotoa FC")
+load_dotenv()
 
+import models
+from database import engine
+from routers import player, club, match, statistic
+from web_routes import pages
 
-@app.get("/")
-async def root():
-    return {"message": "sigmotoa FC data"}
+models.Base.metadata.create_all(bind=engine)
 
+app = FastAPI(title="Sigmotoa FC")
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Bienvenido a sigmotoa FC {name}"}
+templates = Jinja2Templates(directory="templates")
+
+app.state.templates = templates
+
+app.include_router(player.router, prefix="/api")
+app.include_router(club.router, prefix="/api")
+app.include_router(match.router, prefix="/api")
+app.include_router(statistic.router, prefix="/api")
+
+app.include_router(pages.router)
